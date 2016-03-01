f6303bee2371145c942faf76e66b43520c2fd2de
idhooom <dhooom.da@gmail.com>
2016-03-01 15:23:39 +0200

    login, alert action, nice popups

--- unwindsegue/unwindsegue/BackViewController.swift
+++ unwindsegue/unwindsegue/BackViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class BackViewController: UIViewController {
 
+    let alert = UIAlertController(
+        title: "Login required", message: "please enter your Login info", preferredStyle: .Alert)
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -21,6 +25,24 @@ class BackViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
+    @IBAction func login(sender: UIButton) {
+        alert.addTextFieldWithConfigurationHandler{
+            (textField) in
+            textField.placeholder = "Guidance password"
+        }
+        
+        alert.addAction(UIAlertAction(title: "Cancel", style: .Cancel, handler: nil))
+        alert.addAction(UIAlertAction(title: "login", style: .Default) {
+            (action: UIAlertAction) in
+            // get password
+            let tf = (self.alert.textFields?.first)! as UITextField
+            if tf.text != nil {
+                self.title = "loggedIn"
+            }
+            
+            })
+        presentViewController(alert, animated: true, completion: nil)
+    }
    
     
     // MARK: - Navigation
@@ -34,5 +56,6 @@ class BackViewController: UIViewController {
         }
     }
     
+
 
 }
--- unwindsegue/unwindsegue/ViewController.swift
+++ unwindsegue/unwindsegue/ViewController.swift
@@ -19,11 +19,53 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    var loggedIn = false
+    
+    func login(){
+        loggedIn = true
+    }
+    @IBOutlet weak var popUpitem: UIBarButtonItem!
+    
 
+    @IBAction func pop(sender: UIBarButtonItem) {
+        let alert = UIAlertController(
+            title: "Redploy view", message: "just checking if you're okay", preferredStyle: .ActionSheet)
+        let action = UIAlertAction(
+            title: "first", style: .Default){
+                (action: UIAlertAction) -> Void in
+                
+        }
+        let action2 = UIAlertAction(
+            title: "second", style: .Default){
+                (action: UIAlertAction) -> Void in
+                
+        }
+        let action3 = UIAlertAction(
+            title: "third", style: .Destructive){
+                (action: UIAlertAction) -> Void in
+                
+        }
+        let action4 = UIAlertAction(
+            title: "Cancel", style: .Cancel){
+                (action: UIAlertAction) -> Void in
+                
+        }
+        alert.addAction(action)
+        alert.addAction(action2)
+        alert.addAction(action3)
+        alert.addAction(action4)
+
+        // ipad popover presentation in order to tell the popover it's source. otherwise it crashes. 
+        alert.modalPresentationStyle = .Popover
+        let ppc = alert.popoverPresentationController
+        ppc?.barButtonItem = popUpitem
+        
+        presentViewController(alert, animated: true, completion: nil)
+    }
+    
   
 
     @IBAction func goBack(segue: UIStoryboardSegue){
-        print("back")
     }
 }
 
