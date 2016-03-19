b70ad2934d29f9e3f66905f9df0daff39f579400
yoloabdo <dhooom.da@gmail.com>
2016-03-19 23:25:11 +0200

    all set to play, except the ball

--- BreakOut/BreakOut/GameViewController.swift
+++ BreakOut/BreakOut/GameViewController.swift
@@ -14,10 +14,73 @@ class GameViewController: UIViewController {
     
     var blocksPerRow = 8
     
-    var blockkSize: CGSize {
+    var blockSize: CGSize {
         let size = gameView.bounds.size.width / CGFloat(blocksPerRow)
         return CGSize(width: size, height: size/2)
     }
 
+    func block(x: CGFloat, y: CGFloat, color: UIColor){
+        let frame = CGRect(origin: CGPoint(x: x, y: y), size: blockSize)
+        let blockView = UIView(frame: frame)
+        blockView.backgroundColor = color
+        blockView.layer.borderColor = UIColor.blackColor().CGColor
+        blockView.layer.borderWidth = 1.0
+        
+        gameView.addSubview(blockView)
+    }
+    
+    func creatingBlocks(){
+        for row in 0...10 {
+            let color = UIColor.random
+            for col in 0..<blocksPerRow{
+                block(CGFloat(col) * blockSize.width, y: CGFloat(row) * blockSize.height, color: color)
+            }
+        }
+    }
+    
+    func baddle(){
+        let frame = CGRect(
+            x: gameView.bounds.width/2 - blockSize.width,
+            y: gameView.bounds.height - blockSize.height * 2,
+            width: blockSize.width * 2,
+            height: blockSize.height)
+        let baddleView = UIView(frame: frame)
+        baddleView.backgroundColor = UIColor.blackColor()
+        baddleView.layer.borderColor = UIColor.redColor().CGColor
+        gameView.addSubview(baddleView)
+    }
+    
+    func setUpGame() {
+        creatingBlocks()
+        baddle()
+        
+    }
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+     
+        setUpGame()
+    }
+    @IBAction func moveBaddle(sender: UIGestureRecognizer) {
+        switch sender.state {
+        case .Began:
+        }
+    }
 }
 
+
+
+private extension UIColor{
+    class var random: UIColor {
+        switch arc4random() % 7 {
+        case 0: return UIColor.greenColor()
+        case 1: return UIColor.redColor()
+        case 2: return UIColor.blueColor()
+        case 3: return UIColor.orangeColor()
+        case 4: return UIColor.purpleColor()
+        case 5: return UIColor.brownColor()
+        case 6: return UIColor.cyanColor()
+        case 7: return UIColor.yellowColor()
+        default: return UIColor.whiteColor()
+        }
+    }
+}
